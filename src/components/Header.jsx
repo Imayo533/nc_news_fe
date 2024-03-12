0699ff2743a106d382b28556c9da551b6fb44b7f
iam53382@gmail.com <iam53382@gmail.com>
2024-03-12 14:44:24 +0000

    task 4 View a list of all articles completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return(
         <header>
         NC News
+        <nav>
+        <Link to="/" >Home</Link> <br />
+        <Link to="/articles" >Articles</Link>
+        </nav>
       </header>
     )
 }
-export default Header
+export default Header   
